1bf9c13b03452616f6a488ed0377a31190df47bc
Deeptanshu Patel <deeptanshu.patel@gmail.com>
2024-01-26 14:50:21 -0600

    Added zone support

--- frigate2telegram.py
+++ frigate2telegram.py
@@ -1,11 +1,12 @@
-# third party imports which needs installation
-import paho.mqtt.client as mqtt # pip install paho.mqtt
-import yaml # pip install pyyaml
-
 # standard imports
 import json
 import logging
 import urllib.request
+from datetime import datetime, timedelta
+
+# third party imports which needs installation
+import paho.mqtt.client as mqtt # pip install paho.mqtt
+import yaml # pip install pyyaml
 
 # own classes
 from telegram_helper import telegram_helper
@@ -15,6 +16,7 @@ class frigate2telegram:
     def __init__(self):
         logging.basicConfig(format='%(asctime)s.%(msecs)03d %(levelname)s: %(message)s', datefmt='%Y-%m-%d %H:%M:%S', filename = "/logs/frigate2telegram.log", level=logging.INFO)
         self.logger = logging.getLogger(__name__)
+        self.mqtt_event_history = {}
 
     def onMqttConnect(self, client, userdata, flags, rc):
         try:
@@ -30,23 +32,111 @@ class frigate2telegram:
             event_id = json_data['after']['id']
             camera_name = json_data['after']['camera']
             motion_object = json_data['after']['label']
-            self.logger.info(f"camera name: {camera_name}, event type: {event_type}, event id: {event_id}, motion object: {motion_object}")
+            has_clip = json_data["after"]["has_clip"]
+            has_snapshot = json_data["after"]["has_snapshot"]
+            before_entered_zones = json_data["before"]["entered_zones"]
+            entered_zones = json_data["after"]["entered_zones"]
+
+            log_str = f"camera_name: {camera_name}, event_id: {event_id}, event_type: {event_type}, motion_object: {motion_object}, has_snapshot: {has_snapshot}, has_clip: {has_clip}, entered_zones: {entered_zones}, entered_zones(before): {before_entered_zones}"
+
+            found_details = True
+            found_required_zones = self.hasMotionInRequiredZones(camera_name, entered_zones)
+            if found_required_zones is False:
+                log_str = "Ignored: missing zones, " + log_str
+            else:
+                found_details = self.hasDetailsToNotify(event_id, event_type, has_snapshot, has_clip)
+                if found_details is False:
+                    log_str = "Ignored: missing snapshot/clip, " + log_str
+
+            self.logger.info(log_str)
+
+            if found_required_zones is False or found_details is False:
+                return
+
             frigate_url = self.getFrigateUrl(event_type, event_id)
-            if frigate_url != None:
-                status, data = self.readFrigateData(frigate_url)
-                if event_type == "new":
+            if frigate_url is not None:
+                status, data, data_size = self.readFrigateData(frigate_url)
+                if event_type in ["new", "update"]:
                     caption = f"There is a {motion_object} at {camera_name} camera"
-                    self.telegram_helper.sendPhoto(status, caption, data)
+                    self.telegram_helper.sendPhoto(status, caption, data, data_size)
                 elif event_type == "end":
                     caption = f"Video of {motion_object} at {camera_name} camera"
-                    self.telegram_helper.sendVideo(status, caption, data)
+                    self.telegram_helper.sendVideo(status, caption, data, data_size)
         except Exception as ex:
             self.logger.error(f"Exception onMqttMessage: {ex}")
+
+    def hasDetailsToNotify(self, event_id, event_type, has_snapshot, has_clip):
+        can_be_notified = False
+        try:
+            event_history = self.mqtt_event_history.get(event_id)
+            expiry_time = datetime.now() + timedelta(hours=3)
+            if event_type in ["new", "update"]:
+                if event_history is None:
+                    if has_snapshot is True:
+                        self.mqtt_event_history[event_id] = {"sent_snapshot": True, "sent_clip": False, "expiry_time": expiry_time}
+                        can_be_notified = True
+                    else:
+                        self.mqtt_event_history[event_id] = {"sent_snapshot": False, "sent_clip": False, "expiry_time": expiry_time}
+                else:
+                    if event_history["sent_snapshot"] is False and has_snapshot is True:
+                        self.mqtt_event_history[event_id]["sent_snapshot"] = True
+                        can_be_notified = True
+            elif event_type in ["end"]:
+                if event_history is None:
+                    if has_clip is True:
+                        self.mqtt_event_history[event_id] = {"sent_snapshot": True, "sent_clip": True, "expiry_time": expiry_time}
+                        can_be_notified = True
+                    else:
+                        self.mqtt_event_history[event_id] = {"sent_snapshot": True, "sent_clip": False, "expiry_time": expiry_time}
+                else:
+                    if event_history["sent_clip"] is False and has_clip is True:
+                        self.mqtt_event_history[event_id]["sent_clip"] = True
+                        can_be_notified = True
+                        del self.mqtt_event_history[event_id]
+            else:
+                self.logger.error(f"Ignored unexpected event_type: {event_type} reported by MQTT server for event_id: {event_id}")
+            self.purgeTrackedEventHistory()
+        except Exception as ex:
+            self.logger.error(f"Exception hasMotionInRequiredZones: {ex}")
+        self.logger.info(f"Length of tracked event history: {len(self.mqtt_event_history)}")
+        return can_be_notified
+
+    def purgeTrackedEventHistory(self):
+        try:
+            deleted_keys = ""
+            cur_time = datetime.now()
+            for key in list(self.mqtt_event_history.keys()):
+                expiry_time = self.mqtt_event_history[key].get("expiry_time")
+                if expiry_time is not None and expiry_time <= cur_time:
+                    deleted_keys += str(key) + ", "
+                    del self.mqtt_event_history[key]
+            if deleted_keys != "":
+                self.logger.info(f"Deleted tracked event history: {deleted_keys}")
+        except Exception as ex:
+            self.logger.info(f"Exception purgeTrackedEventHistory: {ex}")
+
+    def hasMotionInRequiredZones(self, camera_name, entered_zones):
+        try:
+            camera_list = self.config["frigate"].get("cameras")
+            if camera_list is None:
+                return True
+            camera_details = camera_list.get(camera_name)
+            if camera_details is None:
+                return True
+            zone_list = camera_details.get("zones")
+            if zone_list is None:
+                return True
+            for each_zone in zone_list:
+                if each_zone in entered_zones:
+                    return True
+        except Exception as ex:
+            self.logger.error(f"Exception hasMotionInRequiredZones: {ex}")
+        return False
 
     def getFrigateUrl(self, event_type, event_id):
         try:
             url = None
-            if event_type == "new":
+            if event_type in ["new", "update"]:
                 url = f'http://{self.config["frigate"]["host"]}:{self.config["frigate"]["port"]}/api/events/{event_id}/snapshot.jpg?bbox=1&quality=100&timestamp=1'
             elif event_type == "end":
                 url = f'http://{self.config["frigate"]["host"]}:{self.config["frigate"]["port"]}/api/events/{event_id}/clip.mp4'
@@ -57,14 +147,19 @@ class frigate2telegram:
     def readFrigateData(self, url):
         data = ""
         status = 200
+        data_size = 0
         try:
             with urllib.request.urlopen(url) as f:
                 data = f.read()
+                data_size = data.__sizeof__()
         except Exception as ex:
             status = ex.code
             data = ex.read()
+            data_size = data.__sizeof__()
             self.logger.error(f"Exception readFrigateData: {ex}")
-        return status, data
+        data_size = round(data_size/(1024.0**2), 2)
+        self.logger.info(f"Snapshot/clip size: {data_size} MB for url: {url}")
+        return status, data, data_size
 
     def loadConfig(self):
         try:
@@ -73,13 +168,13 @@ class frigate2telegram:
             with open("/config/config.yaml", "r") as stream:
                 self.config = yaml.safe_load(stream)
                 if self.config["mqtt"]["host"].startswith("_REPLACE_"):
-                    self.logger.error(f"Invalid MQTT host, no messages will be send out")
+                    self.logger.error("Invalid MQTT host, no messages will be send out")
                     status = False
                 if self.config["frigate"]["host"].startswith("_REPLACE_"):
-                    self.logger.error(f"Invalid Frigate host, no messages will be send out")
+                    self.logger.error("Invalid Frigate host, no messages will be send out")
                     status = False
                 if self.config['telegram']['api_token'].startswith("_REPLACE_") or str(self.config['telegram']['chat_id']).startswith("_REPLACE_"):
-                    self.logger.error(f"Invalid telegram token or chat id, no messages will be send out")
+                    self.logger.error("Invalid telegram token or chat id, no messages will be send out")
                     status = False
             return status
         except Exception as ex:
--- telegram_helper.py
+++ telegram_helper.py
@@ -5,9 +5,11 @@ class telegram_helper:
     def __init__(self, config, logger):
         try:
             self.config = config
-            self.logger = logger    
+            self.logger = logger
             self.bot_token = self.config['telegram']['api_token']
             self.bot_chat_id = self.config['telegram']['chat_id']
+            self.max_snapshot_size_mb = int(self.config['telegram']['max_snapshot_size_mb'])
+            self.max_clip_size_mb = int(self.config['telegram']['max_clip_size_mb'])
             self.base_url = "https://api.telegram.org/bot" + self.bot_token
             self.sendMessage("frigate2telegram got started!")
         except Exception as ex:
@@ -18,40 +20,57 @@ class telegram_helper:
             # telegram API: https://core.telegram.org/bots/api#sendmessage
             telegram_message_url = self.base_url + "/sendMessage"
             if message_data != "":
-                data = {"chat_id": self.bot_chat_id, 'text': message_data, "timeout": 30}
-                resp = requests.post(telegram_message_url, data=data)
-                if resp.status_code != 200:
-                    self.logger.error(f"sendMessage error: failed to send message, status: {resp.status_code}, reason: {resp.reason}, text: {resp.text}")
+                data = {"chat_id": self.bot_chat_id, 'text': message_data}
+                resp = requests.post(telegram_message_url, data=data, timeout=30)
+                if resp.status_code == 200:
+                    self.logger.info("Sent message successfully")
+                else:
+                    self.logger.error(f"sendMessage error: failed to send, status: {resp.status_code}, reason: {resp.reason}, text: {resp.text}")
         except Exception as ex:
             self.logger.error(f"Exception sendMessage: {ex}")
 
-    def sendPhoto(self, status, caption, photo_data):
+    def sendPhoto(self, status, caption, photo_data, data_size):
         try:
+            if data_size > self.max_snapshot_size_mb:
+                msg = f"Snapshot is too large {data_size} MB and can't be send for '{caption}'"
+                self.sendMessage(msg)
+                self.logger.error(msg)
+                return
+
             # telegram API: https://core.telegram.org/bots/api#sendphoto
             telegram_photo_url = self.base_url + "/sendPhoto"
             if status == 200 and photo_data != "":
                 files = {'photo': photo_data}
-                data = {"chat_id": self.bot_chat_id, "caption": caption, "timeout": 30}
-                resp = requests.post(telegram_photo_url, data=data, files=files)
-                if resp.status_code != 200:
-                    msg = f"sendPhoto error: failed to send photo, status: {resp.status_code}, reason: {resp.reason}, text: {resp.text}"
-                    self.logger.error(msg)
+                data = {"chat_id": self.bot_chat_id, "caption": caption}
+                resp = requests.post(telegram_photo_url, data=data, files=files, timeout=30)
+                if resp.status_code == 200:
+                    self.logger.info("Sent snapshot successfully")
+                else:
+                    msg = f"sendPhoto error: failed to send, status: {resp.status_code}, reason: {resp.reason}"
+                    self.logger.error(f"{msg}, text: {resp.text}")
                     self.sendMessage(msg)
         except Exception as ex:
             self.logger.error(f"Exception sendPhoto: {ex}")
 
-    def sendVideo(self, status, caption, video_data):
+    def sendVideo(self, status, caption, video_data, data_size):
         try:
+            if data_size > self.max_clip_size_mb:
+                msg = f"Video is too large {data_size} MB and can't be send for '{caption}'"
+                self.sendMessage(msg)
+                self.logger.error(msg)
+                return
+
             # telegram API: https://core.telegram.org/bots/api#sendvideo
             telegram_video_url = self.base_url + "/sendVideo"
             if status == 200 and video_data != "":
                 files = {'video': video_data}
-                data = {"chat_id": self.bot_chat_id, "caption": caption, "timeout": 30}
-                resp = requests.post(telegram_video_url, data=data, files=files)
-                if resp.status_code != 200:
-                    msg = f"sendVideo error: failed to send video, status: {resp.status_code}, reason: {resp.reason}, text: {resp.text}"
-                    self.logger.error(msg)
+                data = {"chat_id": self.bot_chat_id, "caption": caption}
+                resp = requests.post(telegram_video_url, data=data, files=files, timeout=30)
+                if resp.status_code == 200:
+                    self.logger.info("Sent clip successfully")
+                else:
+                    msg = f"sendVideo error: failed to send, status: {resp.status_code}, reason: {resp.reason}"
+                    self.logger.error(f"{msg}, text: {resp.text}")
                     self.sendMessage(msg)
         except Exception as ex:
             self.logger.error(f"Exception sendVideo: {ex}")
-
